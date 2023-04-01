5aff0a2d4cfbcd63d8474c804115c818c33a8782
unaiccz <unaicc2003@gmail.com>
2023-04-01 11:54:05 +0200

    ERROR

--- db/schema.js
+++ db/schema.js
@@ -1,10 +1,17 @@
 const { gql } = require('apollo-server');
 
 const typedefs = gql`
+type Usuario {
+    nombre : String
+    email : String
+    password : String
+    creado : String
 type Query {
 
-    getcurso : String
+    getcurso: String
 }
+type Mutation {
+    nuevouser : Usuario
 
 `;
 module.exports = typedefs
---
+++ models/Usuarios.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const Userschema = mongoose.Schema({
+nombre : {
+    type : String,
+    required : true,
+    trim : true
+},
+email : {
+    type : String,
+    required : true,
+    unique : true,
+},
+password : {
+    type : String,
+    required : true,
+    trim : true
+},
+creado : {
+    type : Date,
+    default : Date.now()
+}
+
+
+
+
+
+});
+module.exports = mongoose.model('Usuario'. Userschema);
